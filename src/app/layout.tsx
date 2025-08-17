b2704d866b7e41f7b9c78142e38dbd48bf83346d
rmvs <nofalrmz@gmail.com>
2025-08-17 20:24:23 +0700

    feat(home): comment out Header component in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
+        {/* <Header /> */}
         <main className="min-h-screen">
           {children}
         </main>
